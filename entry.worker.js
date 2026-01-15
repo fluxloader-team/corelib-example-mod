d9a739ff9d636ad990ccf4ea4df8c52ac93c4b03
Almighty Pumpkin <dracothedragon135@gmail.com>
2026-01-14 17:20:11 -0800

    working worker thing

--- entry.worker.js
+++ entry.worker.js
@@ -1,7 +1,7 @@
-fluxloaderAPI.events.on("cl:soil-dig", (data) => {
+fluxloaderAPI.events.on("cl:cell-change", (data) => {
 	for (const cell of data) {
 		if (cell.toCellType == 0 && cell.fromCellType && cell.fromCellType !== 1 && corelib.exposed.named.soils[cell.fromCellType] === "AlternateFluxite") {
-			fluxloaderAPI.gameInstanceState.environment.postMessage([corelib.exposed.raw.i.dD.IncrementFluxite, cell.loc.x, cell.loc.y]);
+			for (let i = 0; i < 100; i++) fluxloaderAPI.gameInstanceState.environment.postMessage([corelib.exposed.raw.i.dD.IncrementFluxite, cell.loc.x, cell.loc.y]);
 			fluxloaderAPI.gameInstanceState.environment.postMessage([corelib.exposed.raw.i.dD.SpawnFluxiteParticles, cell.loc.x, cell.loc.y]);
 		}
 	}

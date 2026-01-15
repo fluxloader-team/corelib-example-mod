e7347d708857af9314d972ac6b4b02b36163e401
tomster12 <thomas.burniston@gmail.com>
2026-01-15 01:49:35 +0000

    Fixed

--- entry.worker.js
+++ entry.worker.js
@@ -1,6 +1,6 @@
-fluxloaderAPI.events.on("cl:cell-change", (data) => {
+fluxloaderAPI.events.on("cl:soil-dig", (data) => {
 	for (const cell of data) {
-		if (cell.toCellType == 0 && cell.fromCellType && cell.fromCellType !== 1 && corelib.exposed.named.soils[cell.fromCellType] === "AlternateFluxite") {
+		if (cell.toCellType == 0 && cell.cellFromName === "AlternateFluxite") {
 			for (let i = 0; i < 100; i++) fluxloaderAPI.gameInstanceState.environment.postMessage([corelib.exposed.raw.i.dD.IncrementFluxite, cell.loc.x, cell.loc.y]);
 			fluxloaderAPI.gameInstanceState.environment.postMessage([corelib.exposed.raw.i.dD.SpawnFluxiteParticles, cell.loc.x, cell.loc.y]);
 		}
